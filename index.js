2740b65a938699bc1b9698efa940661fdc607136
vexora-0 <bhargav4g13132@gmail.com>
2024-07-01 11:03:11 +0530

    learned about file manipulation

--- index.js
+++ index.js
@@ -4,21 +4,46 @@ const fs = require('fs');
 // Specify the path to the file
 const filePath = path.join(__dirname, 'file.txt');
 
-// Read the file
-fs.readFile(filePath, 'utf8', (err, data) => {
-    if (err) {
-        console.error('Error reading the file:', err);
-        return;
+const dirpath = path.dirname(filePath);
+const extention = path.extname(filePath);
+console.log(dirpath);
+console.log(extention);
+//copy file to different location
+// fs.copyFile(filePath, 'newFolder/file.txt', (err) => {
+//     if (err) {
+//         console.error('Error copying the file:', err);
+//         return;
+//     }
+//     console.log('File copied');
+// });
+
+// // Read the file
+// fs.readFile(filePath, 'utf8', (err, data) => {
+//     if (err) {
+//         console.error('Error reading the file:', err);
+//         return;
+//     }
+//     console.log(data);
+// });
+// // write
+// fs.writeFile('file.txt', "Hello There",'utf-8', (err) => {
+//     console.log('File modified');
+// });
+// // append
+// fs.appendFile('file.txt', 'Hello World', (err, data) => {
+//     if (err) {
+//         console.error('Error appending:', err);
+//         return;
+//     }
+//     console.log(data);
+// });
+
+// const filePath = path.join(__dirname, 'file.txt');
+fs.mkdir('newFolder', (err) => {
+    if (err){
+        console.log('Error creating folder:', err);
     }
-    console.log(data);
+    console.log('Folder created');
 });
-fs.writeFile('file.txt', "Hello There",'utf-8', (err) => {
-    console.log('File modified');
-});
-fs.appendFile('file.txt', 'Hello World', (err, data) => {
-    if (err) {
-        console.error('Error appending:', err);
-        return;
-    }
-    console.log(data);
-});
+
+//// making server
--- makeit.js
+++ makeit.js
@@ -1,12 +1,19 @@
 const path = require('path');
 const fs = require('fs');
 
-// Specify the path to the file
-const dirpath = 'newFolder';
-fs.rm(dirpath, { recursive: true}, (err) => {
-    if (err) {
-        console.error('Error removing the directory:', err);
-        return;
+const filePath = path.join(__dirname, 'file.txt');
+fs.mkdir('newFolder', (err) => {
+    if (err){
+        console.log('Error creating folder:', err);
     }
-    console.log('Directory removed');
-})
+    console.log('Folder created');
+});
+
+// const dirpath = 'newFolder';
+// fs.rm(dirpath, { recursive: true}, (err) => {
+//     if (err) {
+//         console.error('Error removing the directory:', err);
+//         return;
+//     }
+//     console.log('Directory removed');
+// })
